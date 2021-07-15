d5589bd0cb818d8195949fcbfa436aa5fe0f07b5
dekkersbd <79840065+dekkersbd@users.noreply.github.com>
2021-07-15 12:38:45 +0200

    post comments working

--- src/main/java/com/example/horecateamhub/controller/DashboardCommentController.java
+++ src/main/java/com/example/horecateamhub/controller/DashboardCommentController.java
@@ -4,9 +4,11 @@ import com.example.horecateamhub.model.DashboardComment;
 import com.example.horecateamhub.service.DashboardCommentService;
 import com.example.horecateamhub.service.DashboardCommentServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
 import java.util.List;
 
 @CrossOrigin(origins = {"*"} )
@@ -25,8 +27,28 @@ public class DashboardCommentController {
         return dashboardCommentService.findAll();
     }
 
-    @PostMapping
-    public DashboardComment save(@RequestBody DashboardComment dashboardComment) {
-        return dashboardCommentService.save(dashboardComment);
+//    @PostMapping
+//    public DashboardComment save(@RequestBody DashboardComment dashboardComment) {
+//        return dashboardCommentService.save(dashboardComment);
+//    }
+
+    @PostMapping("/new_comment")
+    public ResponseEntity<Object> saveComment(@RequestParam String gebruiker,
+                                              @RequestParam String comment,
+                                              @RequestParam String date)
+{
+        try {
+            DashboardComment dashboardComment = new DashboardComment();
+            dashboardComment.setGebruiker(gebruiker);
+            dashboardComment.setComment(comment);
+            dashboardComment.setDate(date);
+
+
+            dashboardCommentService.save(dashboardComment);
+
+            return ResponseEntity.noContent().build();
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
+        }
     }
 }

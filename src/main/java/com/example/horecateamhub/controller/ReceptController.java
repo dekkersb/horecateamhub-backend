2c3663ec6ad056b288a4425669efff41cb6cd5a1
dekkersbd <79840065+dekkersbd@users.noreply.github.com>
2021-07-28 16:58:00 +0200

    post recipe working

--- src/main/java/com/example/horecateamhub/controller/ReceptController.java
+++ src/main/java/com/example/horecateamhub/controller/ReceptController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = {"*"} )
 @RestController
-@RequestMapping("/recept")
+@RequestMapping("/recepten")
 public class ReceptController {
 private ReceptService receptService;
 
